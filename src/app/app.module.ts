d1118867f408e8cfee876365c75f7f87f0e1c8a9
Jose Dalino <jrdalino@gmail.com>
2021-03-14 07:21:41 +0800

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { AppComponent } from './app.component';
 
@@ -10,8 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,    
+    BrowserModule, 
     AmplifyUIAngularModule    
   ],
   providers: [],
